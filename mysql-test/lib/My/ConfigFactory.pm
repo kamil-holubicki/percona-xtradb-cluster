ae5da94c8b6820581852c06bb32b8075907b61f0
Kamil Holubicki <kamil.holubicki@percona.com>
2023-12-01 12:18:58 +0100

    Merge conflicts solved

--- mysql-test/lib/My/ConfigFactory.pm
+++ mysql-test/lib/My/ConfigFactory.pm
@@ -521,15 +521,7 @@ sub post_check_client_groups {
 }
 
 sub resolve_at_variable {
-<<<<<<< HEAD
-  my ($self, $config, $group, $option, $worker) = @_;
-  local $_ = $option->value();
-  my ($res, $after);
-||||||| merged common ancestors
-  my ($self, $config, $group, $option, $worker) = @_;
-=======
   my ($self, $config, $group, $option) = @_;
->>>>>>> Percona-Server-8.1.0-1
 
   while (m/(.*?)\@((?:\w+\.)+)(#?[-\w]+)/g) {
     my ($before, $group_name, $option_name)= ($1, $2, $3);
@@ -539,31 +531,6 @@ sub resolve_at_variable {
     $group_name =~ s/^\@//; # Remove at
     my $value;
 
-<<<<<<< HEAD
-    if ($group_name =~ "envarray") {
-      $value = $ENV{$option_name.$worker};
-    } elsif ($group_name =~ "env") {
-      $value = $ENV{$option_name};
-    } else {
-      my $from_group= $config->group($group_name)
-        or croak "There is no group named '$group_name' that ",
-          "can be used to resolve '$option_name'";
-      $value= $from_group->value($option_name);
-    }
-    $res .= $before.$value;
-||||||| merged common ancestors
-  my $from;
-  if ($group_name =~ "envarray") {
-    $from = $ENV{$option_name.$worker};
-  } elsif ($group_name =~ "env") {
-    $from = $ENV{$option_name};
-  } else {
-    my $from_group = $config->group($group_name) or
-      croak "There is no group named '$group_name' that ",
-      "can be used to resolve '$option_name'";
-
-    $from = $from_group->value($option_name);
-=======
   my $from;
   if ($group_name =~ "env") {
     $from = $ENV{$option_name};
@@ -573,7 +540,6 @@ sub resolve_at_variable {
       "can be used to resolve '$option_name'";
 
     $from = $from_group->value($option_name);
->>>>>>> Percona-Server-8.1.0-1
   }
   $res .= $after;
 
@@ -586,16 +552,8 @@ sub post_fix_resolve_at_variables {
   foreach my $group ($config->groups()) {
     foreach my $option ($group->options()) {
       next unless defined $option->value();
-<<<<<<< HEAD
-      $self->resolve_at_variable($config, $group, $option, $worker)
-	    if ($option->value() =~ /\@/);
-||||||| merged common ancestors
-      $self->resolve_at_variable($config, $group, $option, $worker)
-        if ($option->value() =~ /^\@/);
-=======
       $self->resolve_at_variable($config, $group, $option)
         if ($option->value() =~ /^\@/);
->>>>>>> Percona-Server-8.1.0-1
     }
   }
 }
