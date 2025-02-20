2a59b8f89d6852e66923c06fc268906495906ffe
Kamil Holubicki <kamil.holubicki@percona.com>
2025-02-20 16:42:12 +0100

    Merge tag 'Percona-Server-9.1.0-1' into pxc-9.1.0-merge
    Percona Server release 9.1.0-1
    
    1. MTR tests conflicts solved
    2. gr and group_replication tests the same as for PS (in general, there
    is no reason to have differences in PXC)

--- scripts/wsrep_sst_xtrabackup-v2.sh
+++ scripts/wsrep_sst_xtrabackup-v2.sh
@@ -126,6 +126,7 @@ declare -a RC
 XTRABACKUP_BIN=xtrabackup
 DATA="${WSREP_SST_OPT_DATA}"
 
+# KH: change this description and verify all 8.0 things
 # Which rolling upgrade combinations do we support?
 # MySql supports following in-place upgrades:
 # 1. 8.0.x (LTS) -> 8.i : 8.0 (LTS) to any 8 innovative
@@ -172,29 +173,29 @@ DATA="${WSREP_SST_OPT_DATA}"
 XTRABACKUP_PATH_PREFIX="$(dirname $0)/pxc_extra/pxb-"
 
 # XB path compatible with the current version of PXC
-XTRABACKUP_THIS_VER_PATH="$(dirname $0)/pxc_extra/pxb-8.4"
+XTRABACKUP_THIS_VER_PATH="$(dirname $0)/pxc_extra/pxb-9.1"
 
 # XB path compatible with prev PXC version. It may be prev Innovative release or LTS
 # if current PXC version is 1st Innovative.
 # Note that this can be the same as XTRABACKUP_PREV_LTS_VER_PATH
-XTRABACKUP_PREV_VER_PATH="$(dirname $0)/pxc_extra/pxb-8.3"
+XTRABACKUP_PREV_VER_PATH="$(dirname $0)/pxc_extra/pxb-8.4"
 
 # XB path compatible previous PXC LTS version
-XTRABACKUP_PREV_LTS_VER_PATH="$(dirname $0)/pxc_extra/pxb-8.0"
+XTRABACKUP_PREV_LTS_VER_PATH="$(dirname $0)/pxc_extra/pxb-8.4"
 
 # Minimum PXB required versions for this node to work
 # To be able to service this version
-XB_THIS_REQUIRED_VERSION="8.4.0"
+XB_THIS_REQUIRED_VERSION="9.1.0"
 # To be able to service previous version
-XB_PREV_REQUIRED_VERSION="8.3.0"
+XB_PREV_REQUIRED_VERSION="8.4.0"
 # To be able to service previous LTS version
-XB_PREV_LTS_REQUIRED_VERSION="8.0.35"
+XB_PREV_LTS_REQUIRED_VERSION="8.4.0"
 
 # Joiner requires Donor to be this LTS version...
-REQUIRED_DONOR_MYSQL_LTS_VERSION="8.0"
+REQUIRED_DONOR_MYSQL_LTS_VERSION="8.4"
 # ...or to be this previous version (note that it may be LTS as well if this is
 # 1st innovative)
-REQUIRED_DONOR_MYSQL_PREV_VERSION="8.3"
+REQUIRED_DONOR_MYSQL_PREV_VERSION="8.4"
 
 # These files carry some important information in form of GTID of the data
 # that is being backed up.
@@ -2407,6 +2408,7 @@ then
             rebuildcmd="--rebuild-indexes --rebuild-threads=$nthreads"
         fi
 
+        # KH: todo: remove
         # We still can receive qpress-compressed backup as the donor may be any 8.0.x
         if test -n "$(find ${DATA} -maxdepth 1 -type f -name '*.qp' -print -quit)"; then
             wsrep_log_info "Compressed qpress files found"
